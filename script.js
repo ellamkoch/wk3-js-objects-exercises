1b797f55bdd8ff2b79acfd85c37ca7aa30b58a7f
Ella <ellamkoch@gmail.com>
2025-10-02 21:28:41 -0500

    2nd exercise done

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ const book = {
 console.log(book.read()); 
 
 
+// Exercise 2
+
+const horse = new Animal("horse", "Monkey", "neiiiigggghhh");
+const dog = new Animal("dog", "Stella", "woof");
+
+function Animal(species, name, sound) {
+    this.species = species;
+    this.name = name;
+    this.sound = sound;
+    this.makeSound = function () {
+        return `${this.name} says ${this.sound}`;
+    }
+};
+
+console.log(horse.makeSound());
+console.log(dog.makeSound());
